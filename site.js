d8abc8e1e3a7c226866cc94ce7417880fbe51c28
Mikel Maron <mikel_maron@yahoo.com>
2013-08-30 11:16:09 -0500

    Create site.js

---
+++ site.js
@@ -0,0 +1,43 @@
+---
+---
+;{% include js/jquery.min.js %}
+
+function createCallback(i) {
+   return function(data) {
+     var preview_image = data.sizes.size[5].source;
+     window.geojson.features[i].properties['image:href'] = preview_image;
+     $("#geojson_output").text( JSON.stringify(window.geojson) );
+     request_count--;  
+     if (request_count == 0) download();
+   };
+}
+
+function doIt() {
+  geojson = JSON.parse($("#geojson_input").text());
+  request_count = geojson.features.length;
+  var flickr_api = "http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=43c27f35a02b6e91c34d1de1590e303d&format=json&format=json&photo_id=";
+    
+  for (var i=0; i < geojson.features.length; i++) {
+    try {
+        var match = geojson.features[i].properties.image.match(/http\:\/\/www\.flickr\.com\/photos\/.*?\/(\d+)/);
+        $.getJSON(flickr_api + match[1] + "&jsoncallback=?", createCallback(i));
+    } catch(err) {
+        request_count--;
+        if (request_count == 0) download();
+    }
+  }
+}
+
+$( "#doIt" ).click(function() {doIt()});
+
+function download() {
+    var blob = new Blob([$("#geojson_output").text()], {type: "application/json"});
+    var url  = URL.createObjectURL(blob);
+
+    var a = document.createElement('a');
+    a.download    = "backup.json";
+    a.href        = url;
+    a.textContent = "Download backup.json";
+
+    document.getElementById('content').appendChild(a);
+}
